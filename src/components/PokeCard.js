cd34dd961c2a81863a585d404214010dc00e58b9
isdilowizeline <76004714+isdilowizeline@users.noreply.github.com>
2021-01-26 20:27:30 -0600

    Implements as Function also

--- src/components/PokeCard.js
+++ src/components/PokeCard.js
@@ -1,5 +1,5 @@
-import React from "react";
 import { mockPokemonData as pokeData } from "../mock/pokeData";
+/*import React from "react";
 class PokeCard extends React.Component {
   render() {
     return (
@@ -15,7 +15,17 @@ class PokeCard extends React.Component {
     );
   }
 }
-export default PokeCard;
-/*
-const PokeCard = () => {};
 export default PokeCard;*/
+const PokeCard = () => (
+  <div>
+    <h1>{pokeData.name}</h1> <br />
+    <img src={pokeData.sprites.front_default} /> <br />
+    <img src={pokeData.sprites.front_shiny} /> <br />
+    <a target="_blank" href={pokeData.video}>
+      Video
+    </a>
+    <br />
+  </div>
+);
+
+export default PokeCard;
